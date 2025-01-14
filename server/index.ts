d60d71f81e0e9b77c0d281a41a416e184d8edd27
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-14 17:19:44 +0200

    add asyncHandler to hadle errors that express cant handle & start buidling the api

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello from index.ts and node 23.6")
+
+
+import express , {RequestHandler} from 'express' 
+import {requestLogger} from './Middlewares/requestLogger'
+import asyncHandler from "express-async-handler"
+
+(
+    async ()=> {
+        const app = express() ; 
+        app.use(express.json())
+        app.use(requestLogger)
+
+        // public endpoints 
+        app.get('/v1/currentWeather'  , asyncHandler(getCityWeather) )
+        app.get('/v1/n-DayWeatherForecast'  , asyncHandler(()=> console.log()) )
+
+
+    }
+) ()
+
+function getCityWeather(...args: unknown[]): void | Promise<void> {
+    throw new Error('Function not implemented.');
+}
