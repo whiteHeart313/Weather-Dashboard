86476c471177b602aa2205503a3f22b1f96c860e
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-15 19:15:56 +0200

    add TODOs

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,23 @@
 
 
 import express  from 'express' 
-import {requestLogger} from './Middlewares/requestLogger'
+import {errHandler, requestLogger} from './Middlewares/loggers'
 import asyncHandler from "express-async-handler"
 import { getCityCurrentWeather , getCityForecastWeather} from './Handlers/cityWeather';
 import dotenv from  'dotenv' ; 
+
+// TODO : Unit Tests for the apis 
+// TODO : Add a cache layer for the api calls
+// TODO : Add a rate limiter for the api calls
+// TODO : Add a docker file for the server
+
 
 (
     async ()=> {
         const app = express() ; 
         app.use(express.json())
         app.use(requestLogger)
+        app.use(errHandler)
         dotenv.config()
         // public endpoints 
         app.get('/v1/currentWeather'  , asyncHandler(getCityCurrentWeather) )
