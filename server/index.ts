d5de24aa0b4c80cccbaf02c8dbcc63b6b313c11c
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-15 17:48:44 +0200

    complete 5-DayWeatherForecast api

--- server/index.ts
+++ server/index.ts
@@ -3,17 +3,18 @@
 import express  from 'express' 
 import {requestLogger} from './Middlewares/requestLogger'
 import asyncHandler from "express-async-handler"
-import { getCityWeather } from './Handlers/cityWeather';
+import { getCityCurrentWeather , getCityForecastWeather} from './Handlers/cityWeather';
+import dotenv from  'dotenv' ; 
 
 (
     async ()=> {
         const app = express() ; 
         app.use(express.json())
         app.use(requestLogger)
-
+        dotenv.config()
         // public endpoints 
-        app.get('/v1/currentWeather'  , asyncHandler(getCityWeather) )
-        app.get('/v1/n-DayWeatherForecast'  , asyncHandler(()=> console.log()) )
+        app.get('/v1/currentWeather'  , asyncHandler(getCityCurrentWeather) )
+        app.get('/v1/5-DayWeatherForecast'  , asyncHandler(getCityForecastWeather) )
 
         app.listen(8080)
         console.log('Server is running http://localhost:8080')
