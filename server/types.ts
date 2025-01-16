fa37aa709465dc0bf2a726b28bda26c7798d1934
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-16 16:54:03 +0200

    formatting

--- server/types.ts
+++ server/types.ts
@@ -1,27 +1,27 @@
 /* Type checking */
 
-import type { RequestHandler } from "express"
+import type { RequestHandler } from "express";
 
-export type  typeValidation<req , res>  = RequestHandler<
-any,
-Partial<message<res>> , 
-Partial<req> , 
-any
-> 
+export type typeValidation<req, res> = RequestHandler<
+  any,
+  Partial<message<res>>,
+  Partial<req>,
+  any
+>;
 
-export type message<T> = T | {message : string }
-export interface cityWeatherResponse { 
-    cityName : string , 
-    currentTemprature : number ,
-    weatherDescription : string ,
-    humidityLevel : number ,
-    windSpeed : number
-    
+export type message<T> = T | { message: string };
+
+export interface cityWeatherResponse {
+  cityName: string;
+  currentTemprature: number;
+  weatherDescription: string;
+  humidityLevel: number;
+  windSpeed: number;
 }
-export type userRequestWeather = Pick<cityWeatherResponse , 'cityName'>
+export type userRequestWeather = Pick<cityWeatherResponse, "cityName">;
 
 export interface DailyForecast {
-    date: string;
-    averageTemperature: string;
-    weatherDescription: string;
+  date: string;
+  averageTemperature: string;
+  weatherDescription: string;
 }
