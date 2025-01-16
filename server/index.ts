db95837aeb7a0c3ab722897c505baa4a2c5c78af
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-16 20:09:07 +0200

    add caching layer

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,16 @@
-import express from "express";
-import { errHandler, requestLogger } from "./Middlewares/loggers";
-import asyncHandler from "express-async-handler";
-import {
-  getCityCurrentWeather,
-  getCityForecastWeather,
-} from "./Handlers/cityWeather";
-import dotenv from "dotenv";
+import express from 'express';
+import { errHandler, requestLogger } from './Middlewares/loggers';
+import asyncHandler from 'express-async-handler';
+import { getCityCurrentWeather, getCityForecastWeather } from './Handlers/cityWeather';
+import dotenv from 'dotenv';
+import { setUpRedis } from './cache/cacheDataStore';
 
 // TODO : Unit Tests for the apis (Partially DONE)
-// TODO : Add a cache layer for the api calls
+// TODO : Add a cache layer for the api calls (DONE , need to TEST)
 // TODO : Add a rate limiter for the api calls
 // TODO : Add a docker file for the server
 
 // Start the server
-async function startServer() {}
-
-startServer();
 
 (async () => {
   const app = express();
@@ -23,14 +18,17 @@ startServer();
   app.use(requestLogger);
   app.use(errHandler);
   dotenv.config();
+
+  await setUpRedis();
+
   // public endpoints
-  app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the weather app");
+  app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the weather app');
   });
-  app.get("/v1/currentWeather", asyncHandler(getCityCurrentWeather));
-  app.get("/v1/5-DayWeatherForecast", asyncHandler(getCityForecastWeather));
+  app.get('/v1/currentWeather', asyncHandler(getCityCurrentWeather));
+  app.get('/v1/5-DayWeatherForecast', asyncHandler(getCityForecastWeather));
 
   app.listen(8080);
 
-  console.log("Server is running http://localhost:8080");
+  console.log('Server is running http://localhost:8080');
 })();
