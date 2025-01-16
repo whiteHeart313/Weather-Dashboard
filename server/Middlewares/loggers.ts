c00385164f050b4ac7af4eb8420c65ac7a2eeb71
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-16 16:54:39 +0200

    formatt logger

--- server/Middlewares/loggers.ts
+++ server/Middlewares/loggers.ts
@@ -1,13 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.log(`${req.method} ${req.path}`);
   next();
 };
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler } from "express";
 
 export const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error('Uncaught exception:', err);
-  res.status(500).send('Oops, an unexpected error occurred, please try again');
+  console.error("Uncaught exception:", err);
+  res.status(500).send("Oops, an unexpected error occurred, please try again");
   next();
 };
