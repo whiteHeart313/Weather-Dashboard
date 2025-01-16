e97291d3b0687dafbce740746b633fd30950e760
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-16 16:52:55 +0200

    format file & add TODOs

--- server/index.ts
+++ server/index.ts
@@ -1,31 +1,36 @@
+import express from "express";
+import { errHandler, requestLogger } from "./Middlewares/loggers";
+import asyncHandler from "express-async-handler";
+import {
+  getCityCurrentWeather,
+  getCityForecastWeather,
+} from "./Handlers/cityWeather";
+import dotenv from "dotenv";
 
-
-import express  from 'express' 
-import {errHandler, requestLogger} from './Middlewares/loggers'
-import asyncHandler from "express-async-handler"
-import { getCityCurrentWeather , getCityForecastWeather} from './Handlers/cityWeather';
-import dotenv from  'dotenv' ; 
-
-// TODO : Unit Tests for the apis 
+// TODO : Unit Tests for the apis (Partially DONE)
 // TODO : Add a cache layer for the api calls
 // TODO : Add a rate limiter for the api calls
 // TODO : Add a docker file for the server
 
+// Start the server
+async function startServer() {}
 
-(
-    async ()=> {
-        const app = express() ; 
-        app.use(express.json())
-        app.use(requestLogger)
-        app.use(errHandler)
-        dotenv.config()
-        // public endpoints 
-        app.get('/v1/currentWeather'  , asyncHandler(getCityCurrentWeather) )
-        app.get('/v1/5-DayWeatherForecast'  , asyncHandler(getCityForecastWeather) )
+startServer();
 
-        app.listen(8080)
-        console.log('Server is running http://localhost:8080')
-    }
-) ()
+(async () => {
+  const app = express();
+  app.use(express.json());
+  app.use(requestLogger);
+  app.use(errHandler);
+  dotenv.config();
+  // public endpoints
+  app.get("/", (req, res) => {
+    res.status(200).send("Welcome to the weather app");
+  });
+  app.get("/v1/currentWeather", asyncHandler(getCityCurrentWeather));
+  app.get("/v1/5-DayWeatherForecast", asyncHandler(getCityForecastWeather));
 
+  app.listen(8080);
 
+  console.log("Server is running http://localhost:8080");
+})();
