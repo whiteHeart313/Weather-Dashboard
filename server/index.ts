9a34e2c0be28f67955a4c010100d2478a19443ae
whiteHeart313 <ammarhamed64@gmail.com>
2025-01-16 20:57:07 +0200

    add rate limitter

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,11 @@ import asyncHandler from 'express-async-handler';
 import { getCityCurrentWeather, getCityForecastWeather } from './Handlers/cityWeather';
 import dotenv from 'dotenv';
 import { setUpRedis } from './cache/cacheDataStore';
+import rateLimit from 'express-rate-limit';
 
 // TODO : Unit Tests for the apis (Partially DONE)
 // TODO : Add a cache layer for the api calls (DONE , need to TEST)
-// TODO : Add a rate limiter for the api calls
+// TODO : Add a rate limiter for the api calls (DONE , need to TEST)
 // TODO : Add a docker file for the server
 
 // Start the server
@@ -18,6 +19,16 @@ import { setUpRedis } from './cache/cacheDataStore';
   app.use(requestLogger);
   app.use(errHandler);
   dotenv.config();
+
+  // Configure rate limiter
+  const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+  });
+
+  // Apply the rate limiter to all API endpoints
+  app.use('/v1/', apiLimiter);
 
   await setUpRedis();
 
